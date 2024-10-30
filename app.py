345d5c51b770b3a231b46c3b14fd13e83579100d
joao.hermenegildo <joao.hermenegildo0107@gmail.com>
2024-10-29 22:08:10 -0300

    29.10 22.08

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ def index():
 
 ####################################################################
 #Programação colaboradores
-
 #region: Colaboradores
 
 class Colaborador(db.Model):
@@ -40,6 +39,8 @@ class Colaborador(db.Model):
     id = db.Column('id_Colaboradores', db.Integer, primary_key=True, nullable=False)
     nome = db.Column('nome_Colaboradores', db.String(100), nullable=True)
     email = db.Column('email_Colaboradores', db.String(100), nullable=True)
+    setor = db.Column('setor_Colaboradores', db.String(100), nullable=True)
+
 
 @app.route('/colaborador')
 def colaborador():
@@ -51,13 +52,14 @@ def adicionar_colaborador():
     if request.method == 'POST':
         nome = request.form['nome']
         email = request.form['email']
+        setor = request.form['setor']
 
         # Validação básica
         if not nome or not email:
             flash('Por favor, preencha todos os campos.')
             return redirect('/adicionar_colaborador')  # Redireciona para o formulário
 
-        novo_colaborador = Colaborador(nome=nome, email=email)
+        novo_colaborador = Colaborador(nome=nome, email=email, setor=setor)
         db.session.add(novo_colaborador)
         db.session.commit()
 
@@ -72,6 +74,7 @@ def editar_colaborador(colaborador_id):
     if request.method == 'POST':
         colaborador.nome = request.form['nome']
         colaborador.email = request.form['email']
+        colaborador.setor = request.form['setor']   
         db.session.commit()
         return redirect(url_for('colaborador', colaborador_id=colaborador.id)) 
         
@@ -85,11 +88,20 @@ def excluir_colaborador(colaborador_id):
     flash('Colaborador excluído com sucesso!')  # Adicione uma mensagem de sucesso
     return redirect(url_for('colaborador'))  # Redireciona para a página de colaboradores
 
+@app.route('/pesquisar_colaborador', methods=['GET'])
+def pesquisar_colaborador():
+    nome = request.args.get('nome', '').strip()
+    if nome:
+        colaboradores = Colaborador.query.filter(Colaborador.nome.like(f"%{nome}%")).all()
+    else:
+        colaboradores = Colaborador.query.all()
+    return render_template('colaborador/Colaborador.html', colaboradores=colaboradores)
+
+
 #endregion
 
 ####################################################################
 #PROGRAMAÇÃO producao
-
 #region: Producao
 
 
@@ -161,12 +173,178 @@ def excluir_producao(producao_id):
         flash(f'Ocorreu um erro ao excluir a produção: {e}', 'danger')
     return redirect(url_for('producao'))
 
+@app.route('/pesquisar_producao', methods=['GET'])
+def pesquisar_producao():
+    nome = request.args.get('nome', '').strip()
+    if nome:
+        producoes = Producao.query.filter(Producao.nome.like(f"%{nome}%")).all()
+    else:
+        producoes = Producao.query.all()
+    return render_template('producao/Producao.html', producoes=producoes)
+
+
+#endregion
+
+####################################################################
+#PROGRAMAÇÃO fornecedor
+#region: Fornecedor
+
+class Fornecedor(db.Model):
+    __tablename__ = 'Fornecedor'
+    id = db.Column('id_Fornecedor', db.Integer, primary_key=True, nullable=False)
+    nome = db.Column('nome_Fornecedor', db.String(100), nullable=False)
+    endereco = db.Column('endereco_Fornecedor', db.String(200), nullable=True)
+    cnpj_pf = db.Column('cnpj_pf_Fornecedor', db.String(18), nullable=True)
+    contato = db.Column('contato_Fornecedor', db.String(100), nullable=True)
+    email = db.Column('email_Fornecedor', db.String(100), nullable=True)
+
+@app.route('/fornecedores')
+def fornecedores():
+    fornecedores = Fornecedor.query.all()
+    return render_template('fornecedor/Fornecedor.html', fornecedores=fornecedores)
+
+@app.route('/adicionar_fornecedor', methods=['GET', 'POST'])
+def adicionar_fornecedor():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        endereco = request.form['endereco']
+        cnpj_pf = request.form['cnpj_pf']
+        contato = request.form['contato']
+        email = request.form['email']
+
+        if not nome:
+            flash('Por favor, preencha o nome do fornecedor.')
+            return redirect('/adicionar_fornecedor')
+
+        novo_fornecedor = Fornecedor(
+            nome=nome,
+            endereco=endereco,
+            cnpj_pf=cnpj_pf,
+            contato=contato,
+            email=email
+        )
+        db.session.add(novo_fornecedor)
+        db.session.commit()
+        flash('Fornecedor adicionado com sucesso!')
+        return redirect('/fornecedores')
+
+@app.route('/editar_fornecedor/<int:fornecedor_id>', methods=['POST'])
+def editar_fornecedor(fornecedor_id):
+    fornecedor = Fornecedor.query.get_or_404(fornecedor_id)
+    if request.method == 'POST':
+        fornecedor.nome = request.form['nome']
+        fornecedor.endereco = request.form['endereco']
+        fornecedor.cnpj_pf = request.form['cnpj_pf']
+        fornecedor.contato = request.form['contato']
+        fornecedor.email = request.form['email']
+        db.session.commit()
+        flash('Fornecedor atualizado com sucesso!')
+        return redirect(url_for('fornecedores', fornecedor_id=fornecedor.id))
+
+@app.route('/excluir_fornecedor/<int:fornecedor_id>', methods=['POST'])
+def excluir_fornecedor(fornecedor_id):
+    try:
+        fornecedor = Fornecedor.query.get_or_404(fornecedor_id)
+        db.session.delete(fornecedor)
+        db.session.commit()
+        flash('Fornecedor excluído com sucesso!', 'success')
+    except Exception as e:
+        flash(f'Ocorreu um erro ao excluir o fornecedor: {e}', 'danger')
+    return redirect(url_for('fornecedores'))
+
+@app.route('/pesquisar_fornecedor', methods=['GET'])
+def pesquisar_fornecedor():
+    nome = request.args.get('nome', '').strip()
+    if nome:
+        fornecedores = Fornecedor.query.filter(Fornecedor.nome.like(f"%{nome}%")).all()
+    else:
+        fornecedores = Fornecedor.query.all()
+    return render_template('fornecedor/Fornecedor.html', fornecedores=fornecedores)
+
+
+#endregion
+
+####################################################################
+#PROGRAMAÇÃO cliente
+#region: Cliente
+class Cliente(db.Model):
+    __tablename__ = 'Cliente'
+    id = db.Column('id_Cliente', db.Integer, primary_key=True, nullable=False)
+    nome = db.Column('nome_Cliente', db.String(100), nullable=False)
+    endereco = db.Column('endereco_Cliente', db.String(200), nullable=True)
+    cnpj_pf = db.Column('cnpj_pf_Cliente', db.String(18), nullable=True)
+    contato = db.Column('contato_Cliente', db.String(100), nullable=True)
+    email = db.Column('email_Cliente', db.String(100), nullable=True)
+
+@app.route('/clientes')
+def clientes():
+    clientes = Cliente.query.all()
+    return render_template('clientes/Clientes.html', clientes=clientes)
+
+@app.route('/adicionar_cliente', methods=['GET', 'POST'])
+def adicionar_cliente():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        endereco = request.form['endereco']
+        cnpj_pf = request.form['cnpj_pf']
+        contato = request.form['contato']
+        email = request.form['email']
+
+        if not nome:
+            flash('Por favor, preencha o nome do cliente.')
+            return redirect('/adicionar_cliente')
+
+        novo_cliente = Cliente(
+            nome=nome,
+            endereco=endereco,
+            cnpj_pf=cnpj_pf,
+            contato=contato,
+            email=email
+        )
+        db.session.add(novo_cliente)
+        db.session.commit()
+        flash('Cliente adicionado com sucesso!')
+        return redirect('/clientes')
+
+@app.route('/editar_cliente/<int:cliente_id>', methods=['POST'])
+def editar_cliente(cliente_id):
+    cliente = Cliente.query.get_or_404(cliente_id)
+    if request.method == 'POST':
+        cliente.nome = request.form['nome']
+        cliente.endereco = request.form['endereco']
+        cliente.cnpj_pf = request.form['cnpj_pf']
+        cliente.contato = request.form['contato']
+        cliente.email = request.form['email']
+        db.session.commit()
+        flash('Cliente atualizado com sucesso!')
+        return redirect(url_for('clientes', cliente_id=cliente.id))
+
+@app.route('/excluir_cliente/<int:cliente_id>', methods=['POST'])
+def excluir_cliente(cliente_id):
+    try:
+        cliente = Cliente.query.get_or_404(cliente_id)
+        db.session.delete(cliente)
+        db.session.commit()
+        flash('Cliente excluído com sucesso!', 'success')
+    except Exception as e:
+        flash(f'Ocorreu um erro ao excluir o cliente: {e}', 'danger')
+    return redirect(url_for('clientes'))
+
+@app.route('/pesquisar_cliente', methods=['GET'])
+def pesquisar_cliente():
+    nome = request.args.get('nome', '').strip()
+    if nome:
+        clientes = Cliente.query.filter(Cliente.nome.like(f"%{nome}%")).all()
+    else:
+        clientes = Cliente.query.all()
+    return render_template('clientes/Clientes.html', clientes=clientes)
+
+
 #endregion
 
 ####################################################################
 #PROGRAMAÇÃO login
-
-#region: login
+#region: Login
 
 class Usuario(db.Model):
     __tablename__ = 'Usuarios'
