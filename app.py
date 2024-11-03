79d9e234344b879580faaed5f438898de0642bbe
joao.hermenegildo <joao.hermenegildo0107@gmail.com>
2024-11-02 21:13:17 -0300

    02.11 21.13

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template, request, redirect, flash, url_for, ses
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from datetime import datetime
+from functools import wraps
 
 #endregion
 
@@ -16,17 +17,28 @@ from datetime import datetime
 #region: Inicio
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secreto'  # Defina uma chave secreta única
-
+app.config['SECRET_KEY'] = 'secreto'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://JOAOHERMENEGILD/FazendaUrbanaLotus?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes'
-
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
 
 @app.route('/')
 def index():
     
     return render_template('index.html')
+
+def verificar_acesso(setores_permitidos):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            usuario_setor = session.get('setor')  # Armazene o setor do usuário na sessão ao fazer login
+            if usuario_setor not in setores_permitidos:
+                flash("Você não tem acesso a essa área, contate seu administrador", "danger")
+                return redirect(url_for('pagina_erro'))  # Redirecionar para uma página de erro ou página inicial
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
 
 #endregion
 
@@ -35,17 +47,20 @@ def index():
 #region: Colaboradores
 
 class Colaborador(db.Model):
-    __tablename__ = 'Colaboradores'
-    id = db.Column('id_Colaboradores', db.Integer, primary_key=True, nullable=False)
-    nome = db.Column('nome_Colaboradores', db.String(100), nullable=True)
-    email = db.Column('email_Colaboradores', db.String(100), nullable=True)
-    setor = db.Column('setor_Colaboradores', db.String(100), nullable=True)
+    __tablename__ = 'Colaborador'
+    id = db.Column('id_Colaborador', db.Integer, primary_key=True, nullable=False)
+    nome = db.Column('nome_Colaborador', db.String(100), nullable=False)
+    email = db.Column('email_Colaborador', db.String(120), unique=True, nullable=False)
+    setor = db.Column('setor_Colaborador', db.String(50), nullable=False)
+    usuario = db.Column('usuario_Colaborador', db.String(50), unique=True, nullable=False)
+    senha = db.Column('senha_Colaborador', db.String(128), nullable=False)  # Armazena a senha criptografada
 
 
 @app.route('/colaborador')
+@verificar_acesso(['Recursos humanos', 'Administrador'])
 def colaborador():
     colaboradores = Colaborador.query.all()
-    return render_template('colaborador/Colaborador.html',colaboradores=colaboradores)
+    return render_template('colaborador/Colaborador.html', colaboradores=colaboradores)
 
 @app.route('/adicionar_colaborador', methods=['GET', 'POST'])
 def adicionar_colaborador():
@@ -53,20 +68,23 @@ def adicionar_colaborador():
         nome = request.form['nome']
         email = request.form['email']
         setor = request.form['setor']
+        usuario = request.form['usuario']
+        senha = request.form['senha']
 
         # Validação básica
-        if not nome or not email:
+        if not nome or not email or not usuario or not senha:
             flash('Por favor, preencha todos os campos.')
             return redirect('/adicionar_colaborador')  # Redireciona para o formulário
 
-        novo_colaborador = Colaborador(nome=nome, email=email, setor=setor)
+        # Criptografando a senha
+        senha_criptografada = bcrypt.generate_password_hash(senha).decode('utf-8')
+
+        novo_colaborador = Colaborador(nome=nome, email=email, setor=setor, usuario=usuario, senha=senha_criptografada)
         db.session.add(novo_colaborador)
         db.session.commit()
 
         flash('Colaborador adicionado com sucesso!')
-        return redirect('/colaborador')  # Redireciona para a página inicial
-
-
+        return redirect('/colaborador')  # Redireciona para a página de colaboradores
 
 @app.route('/editar_colaborador/<int:colaborador_id>', methods=['GET', 'POST'])
 def editar_colaborador(colaborador_id):
@@ -74,10 +92,17 @@ def editar_colaborador(colaborador_id):
     if request.method == 'POST':
         colaborador.nome = request.form['nome']
         colaborador.email = request.form['email']
-        colaborador.setor = request.form['setor']   
+
+
+        # Atualiza a senha somente se um novo valor for fornecido
+        nova_senha = request.form['senha']
+        if nova_senha:
+            colaborador.senha = bcrypt.generate_password_hash(nova_senha).decode('utf-8')
+
         db.session.commit()
-        return redirect(url_for('colaborador', colaborador_id=colaborador.id)) 
-        
+        flash('Colaborador atualizado com sucesso!')  # Mensagem de sucesso ao atualizar
+        return redirect(url_for('colaborador'))  # Redireciona para a página de colaboradores
+
     return render_template('colaborador/item_Colaborador.html', colaborador=colaborador)
 
 @app.route('/excluir_colaborador/<int:colaborador_id>', methods=['POST'])
@@ -85,7 +110,7 @@ def excluir_colaborador(colaborador_id):
     colaborador = Colaborador.query.get_or_404(colaborador_id)
     db.session.delete(colaborador)
     db.session.commit()
-    flash('Colaborador excluído com sucesso!')  # Adicione uma mensagem de sucesso
+    flash('Colaborador excluído com sucesso!')  # Mensagem de sucesso
     return redirect(url_for('colaborador'))  # Redireciona para a página de colaboradores
 
 @app.route('/pesquisar_colaborador', methods=['GET'])
@@ -96,6 +121,33 @@ def pesquisar_colaborador():
     else:
         colaboradores = Colaborador.query.all()
     return render_template('colaborador/Colaborador.html', colaboradores=colaboradores)
+
+@app.route('/alterar_senha', methods=['GET', 'POST'])
+def alterar_senha():
+    if request.method == 'POST':
+        usuario_id = session.get('user_id')  # Obtém o ID do usuário logado
+        colaborador = Colaborador.query.get(usuario_id)  # Busca o colaborador pelo ID
+
+        senha_atual = request.form['senha_atual']
+        nova_senha = request.form['nova_senha']
+
+        # Verifica se a senha atual está correta
+        if bcrypt.check_password_hash(colaborador.senha, senha_atual):
+            # Criptografa a nova senha
+            nova_senha_criptografada = bcrypt.generate_password_hash(nova_senha).decode('utf-8')
+            colaborador.senha = nova_senha_criptografada  # Atualiza a senha
+            db.session.commit()  # Salva as alterações
+            flash('Senha alterada com sucesso!', 'success')
+            return redirect('/home')  # Redireciona para a página inicial
+        else:
+            flash('Senha atual incorreta. Tente novamente.', 'danger')
+
+    return render_template('alterar_senha.html')  # Retorna o template para alterar senha
+
+
+
+
+
 
 
 #endregion
@@ -115,6 +167,7 @@ class Producao(db.Model):
     preco = db.Column('preco_Producao', db.Float, nullable=True)                    # Novo campo
 
 @app.route('/producao')  # Atualizando a rota
+@verificar_acesso(['Producao', 'Administrador'])
 def producao():
     producoes = Producao.query.all()  # Atualizando a consulta para 'Produção'
     return render_template('producao/Producao.html', producoes=producoes)  # Atualizando o template
@@ -199,6 +252,7 @@ class Fornecedor(db.Model):
     email = db.Column('email_Fornecedor', db.String(100), nullable=True)
 
 @app.route('/fornecedores')
+@verificar_acesso(['Vendas', 'Administrador'])
 def fornecedores():
     fornecedores = Fornecedor.query.all()
     return render_template('fornecedor/Fornecedor.html', fornecedores=fornecedores)
@@ -277,6 +331,7 @@ class Cliente(db.Model):
     email = db.Column('email_Cliente', db.String(100), nullable=True)
 
 @app.route('/clientes')
+@verificar_acesso(['Vendas', 'Administrador'])
 def clientes():
     clientes = Cliente.query.all()
     return render_template('clientes/Clientes.html', clientes=clientes)
@@ -346,52 +401,74 @@ def pesquisar_cliente():
 #PROGRAMAÇÃO login
 #region: Login
 
+# Classe do usuário
 class Usuario(db.Model):
     __tablename__ = 'Usuarios'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
 
+# Decorador para verificar login
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        print("Verificando sessão:", session.get('user_id'))  # Verifica se 'user_id' existe
+        if 'user_id' not in session:
+            flash('Por favor, faça login para acessar esta página.', 'warning')
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+# Rota de Registro
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    print("Rota /register acessada")  # Linha para debug
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        # Hash da senha
-        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-
-        # Cria um novo usuário
-        novo_usuario = Usuario(username=username, password=hashed_password)
-        db.session.add(novo_usuario)
-        db.session.commit()
-
-        flash('Usuário registrado com sucesso!', 'success')
-        return redirect(url_for('login'))
-    
+        
     return render_template('cripto/register.html')
 
+# Rota de Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        usuario = request.form['usuario']
+        senha = request.form['senha']
 
-        # Busca o usuário no banco de dados
-        usuario = Usuario.query.filter_by(username=username).first()
-        print(f"Usuário encontrado: {usuario}")  # Debug
+        # Busca o colaborador no banco de dados
+        colaborador = Colaborador.query.filter_by(usuario=usuario).first()
+        
+        if colaborador is not None:  # Verifica se o colaborador foi encontrado
+            print(f"Colaborador encontrado: {colaborador.usuario}")  # Debug
 
-        # Verifica se o usuário existe e se a senha está correta
-        if usuario and bcrypt.check_password_hash(usuario.password, password):
-            session['user_id'] = usuario.id  # Armazena o ID do usuário na sessão
-            flash('Login realizado com sucesso!', 'success')
-            return redirect(url_for('home'))  # Redireciona para a rota "home"
+            # Verifica a senha
+            if bcrypt.check_password_hash(colaborador.senha, senha):
+                session['user_id'] = colaborador.id  # Armazena o ID do colaborador na sessão
+                session['setor'] = colaborador.setor  # Armazena o setor do colaborador na sessão
+                print("Sessão configurada com ID do colaborador:", session.get('user_id'))  # Debug
+                print("Setor do colaborador:", session.get('setor'))  # Debug
+                flash('Login realizado com sucesso!', 'success')
+                return redirect(url_for('home'))
+            else:
+                flash('Senha incorreta.', 'danger')
         else:
-            flash('Usuário ou senha incorretos.', 'danger')
-            print("Usuário ou senha incorretos.")  # Debug
+            flash('Colaborador não encontrado.', 'danger')
 
     return render_template('cripto/login.html')
+
+
+
+# Rota para Logout
+@app.route('/logout')
+def logout():
+    session.clear()  # Limpa a sessão ao fazer logout
+    flash('Você saiu com sucesso.', 'info')
+    return redirect(url_for('login'))
+
+
+
+@app.route('/pagina_erro')
+def pagina_erro():
+    return render_template('cripto/erroAcesso.html') 
+
 
 
 #endregion
@@ -401,9 +478,16 @@ def login():
 
 #region: Home
 
+# Rota Home protegida com verificação direta
 @app.route('/home')
 def home():
+    if 'user_id' not in session:
+        flash('Por favor, faça login para acessar esta página.', 'warning')
+        return redirect(url_for('login'))
+    
+    # Exibe a página home normalmente se o usuário estiver logado
     return render_template('home.html')
+
 
 #endregion
 
